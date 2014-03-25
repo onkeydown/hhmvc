14a2a3c6f81b80ad674fb3b9b4f6516dc26e85ba
onkeydown <onkeydown@gmail.com>
2014-03-25 20:43:00 +0100

    missing cache folder

---
+++ cache/templates_c/159dc58c497ecaee04b7a662fd77777e382c70fe.file.script.tpl.cache.php
@@ -0,0 +1,23 @@
+<?php /* Smarty version Smarty-3.1.17, created on 2014-03-25 19:37:03
+         compiled from "S:\xampp\htdocs\hhmvc\public\themes\default\basic\script.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:286625331dadf00ce49-70920456%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '159dc58c497ecaee04b7a662fd77777e382c70fe' => 
+    array (
+      0 => 'S:\\xampp\\htdocs\\hhmvc\\public\\themes\\default\\basic\\script.tpl',
+      1 => 1395542725,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '286625331dadf00ce49-70920456',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.17',
+  'unifunc' => 'content_5331dadf010cd9_00462903',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5331dadf010cd9_00462903')) {function content_5331dadf010cd9_00462903($_smarty_tpl) {?><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<?php }} ?>
